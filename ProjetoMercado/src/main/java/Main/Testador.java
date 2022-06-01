1260e1aee8f92102224b02d53dcd5ca35290e8ce
matheus98588 <matheuspimentelpilotto@gmail.com>
2022-06-01 11:25:35 -0300

    comentando codigo no testo

--- ProjetoMercado/src/main/java/Main/Testador.java
+++ ProjetoMercado/src/main/java/Main/Testador.java
@@ -1,5 +1,7 @@
 package Main;
 
+import java.util.Calendar;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -10,5 +12,18 @@ package Main;
  * @author pcezar
  */
 public class Testador {
-    P
+    public static void main(String[] args) {
+        Prateleira teste =new  Prateleira();
+        Calendar c = Calendar.getInstance();
+        c.getTime();
+        teste.addProdPrateleira("feijao", 8, c, 1);
+        teste.addProdPrateleira("arroz", 10, c, 2);
+        teste.addProdPrateleira("lasanha", 30, c, 3);
+        teste.organizaPrateleira();
+        System.out.println(teste.getProdutos().get(0).getNome());
+        System.out.println(teste.getProdutos().get(1).getNome());
+        System.out.println(teste.getProdutos().get(2).getNome());
+
+
+    }
 }
