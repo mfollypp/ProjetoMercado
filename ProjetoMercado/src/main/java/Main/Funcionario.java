2c08f19d0120029604a1067c03658f7c2173d735
Matheus Folly <matheusfolly@id.uff.br>
2022-06-22 09:22:34 -0300

    Implementando arquivo funcionarios

--- ProjetoMercado/src/main/java/Main/Funcionario.java
+++ ProjetoMercado/src/main/java/Main/Funcionario.java
@@ -1,10 +1,14 @@
 package Main;
 
-public class Funcionario {
+import java.io.Serializable;
+
+public class Funcionario implements Serializable {
+    private String nome;
     private int cadastro;
     private String tipoFuncionario;
 
-    public Funcionario(int cadastro, String tipoFuncionario) {
+    public Funcionario(String nome, int cadastro, String tipoFuncionario) {
+        this.nome = nome;
         this.cadastro = cadastro;
         this.tipoFuncionario = tipoFuncionario;
     }
@@ -23,6 +27,11 @@ public class Funcionario {
 
     public String getTipoFuncionario() {
         return tipoFuncionario;
+    }
+    
+    @Override
+    public String toString() {
+	return "Nome:" + this.nome + "\nCadastro: " + this.cadastro + "\nTipo Funcionario: " + this.tipoFuncionario;
     }
     
 }
--- ProjetoMercado/src/main/java/Main/Mercado.java
+++ ProjetoMercado/src/main/java/Main/Mercado.java
@@ -1,18 +1,68 @@
 package Main;
 
 
+import java.io.EOFException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
 public class Mercado {
     private String endereco;
     private ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
     private ArrayList<Corredor> corredores = new ArrayList<Corredor>();
     private Calendar data;
+    private FileOutputStream fos = null;
+    private ObjectOutputStream ous = null;
+    private FileInputStream fis = null;
+    private ObjectInputStream ois = null;
 
     public Mercado(String endereco) {
         this.endereco = endereco;
         this.data = Calendar.getInstance();
+    }
+    
+    public void addFuncionario(String nome, int cadastro, String tipoFuncionario) throws IOException{
+        try{
+            Funcionario func = new Funcionario(nome, cadastro, tipoFuncionario);
+            fos = new FileOutputStream("funcionarios.txt", true);
+            ous = new ObjectOutputStream(fos);
+            ous.writeObject(func);
+        }catch (IOException ex){
+            System.out.println("Erro ao escrever no arquivo");
+        }
+        finally{
+            if(fos != null){
+                fos.close();
+            }
+            if(ous != null){
+                ous.close();
+            }
+        }
+    }
+    
+    public void getFuncionarios() throws FileNotFoundException, IOException, ClassNotFoundException, EOFException{ // ta perdendo o header do output stream
+        try{
+            fis = new FileInputStream("funcionarios.txt");
+            ois = new ObjectInputStream(fis);
+            while(true){
+                Funcionario func = (Funcionario) ois.readObject();
+                System.out.println(func.toString());
+            }
+        }catch (EOFException e){
+            System.out.println("Fim do arquivo");
+        }finally{
+            if(fis != null){
+                fis.close();
+            }
+            if(ois != null){
+                ois.close();
+            }
+        }
     }
     
     public void passarDia(){
@@ -23,9 +73,9 @@ public class Mercado {
         this.endereco = endereco;
     }
 
-    public void setFuncionarios(ArrayList<Funcionario> funcionarios) {
-        this.funcionarios = funcionarios;
-    }
+//    public void setFuncionarios(ArrayList<Funcionario> funcionarios) {
+//        this.funcionarios = funcionarios;
+//    }
 
     public void setCorredores(ArrayList<Corredor> corredores) {
         this.corredores = corredores;
@@ -39,9 +89,9 @@ public class Mercado {
         return endereco;
     }
 
-    public ArrayList<Funcionario> getFuncionarios() {
-        return funcionarios;
-    }
+//    public ArrayList<Funcionario> getFuncionarios() {
+//        return funcionarios;
+//    }
 
     public ArrayList<Corredor> getCorredores() {
         return corredores;
